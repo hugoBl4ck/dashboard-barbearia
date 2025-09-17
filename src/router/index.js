ab4ad199f2f3e070eb7a738211f4b1330580da3f
unknown <hugovieira.eng@gmail.com>
2025-09-17 20:22:40 -0300

    tentativa de carregar os dados da barbearia 1

--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,48 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
 import ClientesView from '../views/ClientesView.vue'
 import AnalyticsView from '../views/AnalyticsView.vue'
 import ChatView from '../views/ChatView.vue'
-import ClientLandingPageView from '../views/ClientLandingPageView.vue'
-
+import { authGuard, guestGuard } from './authGuard.js'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/login',
+      name: 'Login',
+      component: LoginView,
+      beforeEnter: guestGuard
+    },
+    {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      beforeEnter: authGuard
+    },
+    {
+      path: '/dashboard',
+      redirect: '/'
     },
     {
       path: '/clientes',
       name: 'clientes',
-      component: ClientesView
+      component: ClientesView,
+      beforeEnter: authGuard
     },
     {
       path: '/analytics',
       name: 'analytics',
-      component: AnalyticsView
+      component: AnalyticsView,
+      beforeEnter: authGuard
     },
     {
       path: '/chat',
       name: 'chat',
-      component: ChatView
-    },
-    {
-      path: '/agendar',
-      name: 'agendar',
-      component: ClientLandingPageView
+      component: ChatView,
+      beforeEnter: authGuard
     }
-    
   ]
 })
 
